adf43b903082dbb3ffd87a41894cf6a1476195df
Gustavo <125235805+DnzGus@users.noreply.github.com>
2024-02-15 19:16:01 -0300

    Rework

--- TwoSum.php
+++ TwoSum.php
@@ -1,18 +1,13 @@
 <?php
 
 function twoSum($nums, $target) {
-  $len = count($nums);
-
-  for($i = 0; $i < $len; $i++){
-    for($j = 1; $j < $len; $j++){
-      if($i == $j){
-        continue;
-      }
-      $sum = $nums[$i] + $nums[$j];
-      if($sum == $target){
-        $arr = [$i,$j];
-        return $arr;
-    }
-    }
+ $arr = [];
+ $len = count($nums);
+ foreach($nums as $key => $val){
+  $sub = $target - $val;
+  if(isset($arr[$sub])){
+   return [$arr[$sub], $key];
   }
+  $arr[$val] = $key;
+ }
 }
